dfc77ff86444fd2ac5bd447c2be44d774727fc69
ci-automation <ci.cnabu@vmware.com>
2019-07-31 22:54:08 -0700

    add doc

--- get-pod-error-log.sh
+++ get-pod-error-log.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+##  @copyright 2019 DennyZhang.com
+## Licensed under MIT 
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: delete_all_containers.sh
+## Author : Denny <https://www.dennyzhang.com/contact>
+## Description : https://cheatsheet.dennyzhang.com/cheatsheet-kubernetes-A4
+## --
+## Created : <2018-07-10>
+## Updated: Time-stamp: <2019-07-31 22:54:01>
+##-------------------------------------------------------------------
+# curl -L https://raw.githubusercontent.com/dennyzhang/cheatsheet-kubernetes-A4/master/get-pod-error-log.sh | bash
 namespace=${1:-""}
 log_pattern="grep -iE 'exception|error' | grep -v 'Reconciler error' | tail -n10"
 
